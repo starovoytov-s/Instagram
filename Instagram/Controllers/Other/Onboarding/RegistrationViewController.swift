c42f0504388d1192c8756c104081b06ba8de2620
Stanislav <63556577+starovoytov-s@users.noreply.github.com>
2020-09-18 12:36:26 +0300

    Part three

--- Instagram/Controllers/Other/Onboarding/RegistrationViewController.swift
+++ Instagram/Controllers/Other/Onboarding/RegistrationViewController.swift
@@ -12,7 +12,9 @@ class RegistrationViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+       
+        view.backgroundColor = .systemBackground
+        
     }
     
 }
